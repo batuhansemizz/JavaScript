66e2bf4edb3f4451f9737d1bcc7578fbfe1a3ea9
Batuhan Semiz <batuhansemiz52@gmail.com>
2024-09-05 23:18:45 +0300

    mesaj

--- elementleriDeğiştirmek.js
+++ elementleriDeğiştirmek.js
@@ -1,14 +1,9 @@
-// Elementleri Değiştirmek
+//! Elementlerde yer değişimi 
 
 const cardBody = document.querySelectorAll(".card-body")[1];
 
-// <h5 class="card-title">Todo Listesi</h5>
-
-const newTitle = document.createElement("h2");
+const newTitle = document.createElement("h2")
 newTitle.className="card-title";
-newTitle.textContent = "Todo Listesi - Yeni";""
-
-
+newTitle.textContent="todo listesi - yeni";
 
 cardBody.replaceChild(newTitle,cardBody.childNodes[1]);
-
